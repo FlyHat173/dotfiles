d7bb0bbac9e8e678b8c998c4ec208edf209d1429
FlyHat173 <phamthanhphat1703@gmail.com>
2025-08-31 15:33:16 +0700

    update

--- nvim/init.lua
+++ nvim/init.lua
@@ -26,3 +26,105 @@ vim.keymap.set("n", "<leader>fc", function()
   })
 end, { desc = "Find Config Files" })
 
+
+--colorscheme
+require("catppuccin").setup({
+    flavour = "auto", -- latte, frappe, macchiato, mocha
+    background = { -- :h background
+        light = "latte",
+        dark = "mocha",
+    },
+    transparent_background = false, -- disables setting the background color.
+    float = {
+        transparent = true, -- enable transparent floating windows
+        solid = false, -- use solid styling for floating windows, see |winborder|
+    },
+    show_end_of_buffer = false, -- shows the '~' characters after the end of buffers
+    term_colors = true, -- sets terminal colors (e.g. `g:terminal_color_0`)
+    dim_inactive = {
+        enabled = false, -- dims the background color of inactive window
+        shade = "dark",
+        percentage = 0.15, -- percentage of the shade to apply to the inactive window
+    },
+    no_italic = false, -- Force no italic
+    no_bold = false, -- Force no bold
+    no_underline = false, -- Force no underline
+    styles = { -- Handles the styles of general hi groups (see `:h highlight-args`):
+        comments = { "italic" }, -- Change the style of comments
+        conditionals = { "italic" },
+        loops = {},
+        functions = {},
+        keywords = {},
+        strings = {},
+        variables = {},
+        numbers = {},
+        booleans = {},
+        properties = {},
+        types = {},
+        operators = {},
+        -- miscs = {}, -- Uncomment to turn off hard-coded styles
+    },
+    color_overrides = {},
+    custom_highlights = {},
+    default_integrations = true,
+    auto_integrations = false,
+    integrations = {
+        cmp = true,
+        gitsigns = true,
+        nvimtree = true,
+        treesitter = true,
+        notify = false,
+        mini = {
+            enabled = true,
+            indentscope_color = "",
+        },
+        -- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
+    },
+})
+
+-- setup must be called before loading
+vim.cmd.colorscheme "catppuccin"
+
+--noice plugins
+require("noice").setup({
+  lsp = {
+    -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+    override = {
+      ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+      ["vim.lsp.util.stylize_markdown"] = true,
+      ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
+    },
+  },
+  -- you can enable a preset for easier configuration
+  presets = {
+    bottom_search = true, -- use a classic bottom cmdline for search
+    command_palette = true, -- position the cmdline and popupmenu together
+    long_message_to_split = true, -- long messages will be sent to a split
+    inc_rename = false, -- enables an input dialog for inc-rename.nvim
+    lsp_doc_border = false, -- add a border to hover docs and signature help
+  },
+})
+
+
+--dashboard
+require("dashboard").setup ({
+      theme = "doom",
+      config = {
+        header = {
+'██╗      █████╗ ██╗   ██╗ ██████╗ ██╗  ██╗',
+'██║     ██╔══██╗██║   ██║██╔════╝ ██║  ██║',
+'██║     ███████║██║   ██║██║  ███╗███████║',
+'██║     ██╔══██║██║   ██║██║   ██║██╔══██║',
+'███████╗██║  ██║╚██████╔╝╚██████╔╝██║  ██║',
+'╚══════╝╚═╝  ╚═╝ ╚═════╝  ╚═════╝ ╚═╝  ╚═╝',
+        },
+        center = {
+          { icon = "  ",     desc = "New File",          action = "enew",                      key = "n" },
+          { icon = "  ",     desc = "Find File",         action = "Telescope find_files",      key = "f" },
+          { icon = "  ",     desc = "Recent Files",      action = "Telescope oldfiles",        key = "r" },
+          { icon = "  ",     desc = "Quit",              action = "qa",                        key = "q" },
+          { icon = "   ",  desc = "config file",       action = "Telescope find_files cwd=~/.config/nvim",     key = "c"},
+        },
+        footer = { "⚡hehe ⚡" },
+      },
+    })

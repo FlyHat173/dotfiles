af4d8c5dbe2b8ba69b7258540acb59cffd3ce33e
FlyHat173 <phamthanhphat1703@gmail.com>
2026-03-12 19:31:04 +0700

    update

--- my game/sudoku/prettier.config.js
+++
@@ -1,7 +0,0 @@
-export default {
-  semi: true,
-  printWidth: 80,
-  tabWidth: 2,
-  plugins: ["prettier-plugin-tailwindcss"],
-  tailwindConfig: "./tailwind.config.js",
-}
--- my game/sudoku/src/pages/Game/shortcuts/ShortcutScope.ts
+++
@@ -1,4 +0,0 @@
-export enum ShortcutScope {
-  Game = "Game",
-  Menu = "Menu",
-}

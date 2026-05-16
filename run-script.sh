513b33dc2cd623c590da6c24d61122237b8ee431
FlyHat173 <phamthanhphat1703@gmail.com>
2026-05-16 19:28:49 +0700

    update

---
+++ run-script.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+SCRIPTS_DIR="$HOME/dotfiles/script-manager/.config/script-manager"
+
+chosen=$(ls "$SCRIPTS_DIR" | rofi -dmenu -p "▶ Run script")
+[ -z "$chosen" ] && exit 0
+
+script="$SCRIPTS_DIR/$chosen"
+chmod +x "$script"
+
+# i3-sensible-terminal tự chọn terminal bạn đang dùng
+i3-sensible-terminal -e bash -c "$script; echo; read -rp 'Nhấn Enter để đóng...'"
---
+++ script-manager/.config/script-manager/meme.sh
@@ -0,0 +1,6 @@
+!#/bin/bash 
+
+cd ~/MEME 
+git add .
+git commit -m 'update'
+git push -u origin main
